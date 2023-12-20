644e9e96eafb646ea10ec7350022e31ed1ceed42
NAGMDRS <narsina.akshayguptha@gmail.com>
2023-12-20 15:26:17 +0530

    Line segmentation done

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 
 def sharpenImage(im):  # Need to check if sharpening input images would help accuracy
-    kernel = np.ones((3,3), np.float32) / 90
+    kernel = np.ones((3, 3), np.float32) / 90
     im = cv2.filter2D(im, -1, kernel)  # -1 means depth of image remains unchanged
     im = cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     im = 255 - im
@@ -34,32 +34,22 @@ def alignImg(im):
 img = alignImg(img)
 
 pixelSum = np.sum(img == 255, axis=1)
-rows=[]
-seg=[]
-
-count=0
-mini=1000
+rows = []
+seg = []
 
 for i in range(len(pixelSum)):
-    mini=min(mini,pixelSum[i])
-    if(pixelSum[i]<=250):
-        count=count+1
-
-
-    if(pixelSum[i]>250):
+    if pixelSum[i] > 20:
         seg.append(i)
 
-    if(pixelSum[i]<=250) & (len(seg)>=100):
+    if (pixelSum[i] <= 20) & (len(seg) >= 20):
         rows.append(seg)
-        seg=[]
+        seg = []
 
-    if(len(seg)>0):
+    if len(seg) > 40:
         rows.append(seg)
-        seg=[]
+        seg = []
 
-print("NO OF LINES",len(rows))
-print(count,mini)
+print("NO OF LINES", len(rows))
 
-img2 = cv2.imread("DATA/ocrTestImage.jpeg", 0)
-plt.imshow(img2)
+plt.imshow(img[rows[5][0]:rows[5][-1],:])
 plt.show()
