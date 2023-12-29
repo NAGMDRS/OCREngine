3d2af053e2d632f6d91bb5a30d5d0625d53ed126
NAGMDRS <narsina.akshayguptha@gmail.com>
2023-12-29 23:47:41 +0530

    NN added

--- Model.py
+++ Model.py
@@ -1,7 +1,9 @@
 import string
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional
+from keras.models import Model
+import keras.backend as K
 
 recognizableChar = string.digits + string.ascii_letters + string.punctuation
 
@@ -34,5 +36,32 @@ def reshapeForNN(img, size):
     return canvas
 
 
-img = cv2.imread("DATA/sharpend.jpeg", 0)
-reshapeForNN(img, size)
+input = Input(shape=(32, 128, 1))
+conv_1 = Conv2D(64, (3, 3), activation='relu', padding='same')(input)
+pool_1 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_1)
+
+conv_2 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool_1)
+pool_2 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_2)
+
+conv_3 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool_2)
+
+conv_4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv_3)
+pool_4 = MaxPool2D(pool_size=(2, 1))(conv_4)
+
+conv_5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool_4)
+batch_norm_5 = BatchNormalization()(conv_5)
+
+conv_6 = Conv2D(512, (3, 3), activation='relu', padding='same')(batch_norm_5)
+batch_norm_6 = BatchNormalization()(conv_6)
+pool_6 = MaxPool2D(pool_size=(2, 1))(batch_norm_6)
+
+conv_7 = Conv2D(512, (2, 2), activation='relu')(pool_6)
+
+squeezed = Lambda(lambda x: K.squeeze(x, 1))(conv_7)
+
+blstm_1 = Bidirectional(LSTM(128, return_sequences=True, dropout=0.2))(squeezed)
+blstm_2 = Bidirectional(LSTM(128, return_sequences=True, dropout=0.2))(blstm_1)
+
+outputs = Dense(len(recognizableChar) + 1, activation='softmax')(blstm_2)
+
+act_model = Model(input, outputs)
