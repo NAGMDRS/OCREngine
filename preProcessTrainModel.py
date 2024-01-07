263a751b08f0f652e81d784a2dfeec4a54317971
NAGMDRS <narsina.akshayguptha@gmail.com>
2024-01-08 01:13:21 +0530

    model and training data

---
+++ preProcessTrainModel.py
@@ -0,0 +1,148 @@
+import string
+from random import random
+import cv2
+import numpy as np
+from keras.src.utils import pad_sequences
+from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional
+from keras.models import Model
+import keras.backend as K
+
+recognizableChar = string.digits + string.ascii_letters + string.punctuation
+size = (32, 128)
+
+
+def encodeText(txt):
+    digitList = []
+    for char in txt:
+        try:
+            digitList.append(recognizableChar.index(char))
+        except:
+            print(char)
+
+    return digitList
+
+
+def sharpenImage(im):  # Need to check if sharpening input images would help accuracy
+    kernel = np.ones((4, 2), np.float32) / 90  # (3,3) felt meh...
+    im = cv2.filter2D(im, -1, kernel)  # -1 means depth of image remains unchanged
+    im = cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 1)
+    im = 255 - im
+    # cv2.imwrite("DATA/sharpend1.jpeg",im)
+    return im
+
+
+def reshapeForNN(img, size):
+    (H, W) = size
+    (h, w) = img.shape
+    R = min(W / w, H / h)
+    img = cv2.resize(img, (int(w * R), int(h * R)), interpolation=cv2.INTER_CUBIC)
+    canvas = np.zeros(size)
+    npImg = np.asarray(img)
+    for j in range(0, int(w * R) - 1):
+        for i in range(0, int(h * R) - 1):
+            canvas[i][j] = max(canvas[i][j], npImg[i][j])
+    cv2.imwrite("DATA/Test.jpeg", canvas)
+    return canvas
+
+
+# lists for training dataset
+training_img = []
+training_txt = []
+train_input_length = []
+train_label_length = []
+orig_txt = []
+
+# lists for validation dataset
+valid_img = []
+valid_txt = []
+valid_input_length = []
+valid_label_length = []
+valid_orig_txt = []
+
+max_label_len = 0
+
+annot = open('F:/OCR/Data-generator-for-CRNN/annotation.txt', 'r').readlines()
+imagenames = []
+txts = []
+
+for cnt in annot:
+    filename, txt = cnt.split(',')[0], cnt.split(',')[1].split('\n')[0]
+    imagenames.append(filename)
+    txts.append(txt)
+
+c = list(zip(imagenames, txts))
+
+random.shuffle(c)
+
+imagenames, txts = zip(*c)
+
+for i in range(len(imagenames)):
+    img = cv2.imread('/content/Data-generator-for-CRNN/images/' + imagenames[i], 0)
+
+    img = sharpenImage(img)
+    img = reshapeForNN(img, size)
+    img = np.expand_dims(img, axis=-1)
+    img = img / 255.
+    txt = txts[i]
+
+    # compute maximum length of the text
+    if len(txt) > max_label_len:
+        max_label_len = len(txt)
+
+    # split the 150000 data into validation and training dataset as 10% and 90% respectively
+    if i % 10 == 0:
+        valid_orig_txt.append(txt)
+        valid_label_length.append(len(txt))
+        valid_input_length.append(31)
+        valid_img.append(img)
+        valid_txt.append(encodeText(txt))
+    else:
+        orig_txt.append(txt)
+        train_label_length.append(len(txt))
+        train_input_length.append(31)
+        training_img.append(img)
+        training_txt.append(encodeText(txt))
+
+# wordNow --> wordNow00000
+train_padded_txt = pad_sequences(training_txt, maxlen=max_label_len, padding='post', value=len(recognizableChar))
+valid_padded_txt = pad_sequences(valid_txt, maxlen=max_label_len, padding='post', value=len(recognizableChar))
+
+# CRNN model
+inputs = Input(shape=(32, 128, 1))
+
+# convolution layer with kernel size (3,3)
+conv_1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+# pooling layer with kernel size (2,2)
+pool_1 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_1)
+
+conv_2 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool_1)
+pool_2 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_2)
+
+conv_3 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool_2)
+
+conv_4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv_3)
+# poolig layer with kernel size (2,1)
+pool_4 = MaxPool2D(pool_size=(2, 1))(conv_4)
+
+conv_5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool_4)
+# Batch normalization layer
+batch_norm_5 = BatchNormalization()(conv_5)
+
+conv_6 = Conv2D(512, (3, 3), activation='relu', padding='same')(batch_norm_5)
+batch_norm_6 = BatchNormalization()(conv_6)
+pool_6 = MaxPool2D(pool_size=(2, 1))(batch_norm_6)
+
+conv_7 = Conv2D(512, (2, 2), activation='relu')(pool_6)
+
+squeezed = Lambda(lambda x: K.squeeze(x, 1))(conv_7)
+
+# bidirectional LSTM layers with units=128
+blstm_1 = Bidirectional(LSTM(128, return_sequences=True, dropout=0.2))(squeezed)
+blstm_2 = Bidirectional(LSTM(128, return_sequences=True, dropout=0.2))(blstm_1)
+
+outputs = Dense(len(recognizableChar) + 1, activation='softmax')(blstm_2)
+
+# model to be used at test time
+act_model = Model(inputs, outputs)
+
+
